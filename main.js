289d05bd3f18ccfa37492a8dc12982b9348e21bc
seoyeon <jungluchia1204@gmail.com>
2022-04-22 16:15:39 +0900

    [Refactor] JS 코드 수정

--- main.js
+++ main.js
@@ -1,78 +1,148 @@
-document.addEventListener('DOMContentLoaded', () => {
+let addBtn = document.querySelector('.addBtn');  // 추가버튼
+let inputTxt = document.querySelector('.inputTxt');  // 할일 입력창
+let input_box = document.querySelector('.input_box');  // 할일입력칸
+let delTodo = document.querySelectorAll('.delBtn');  // 할일삭제
+let todoList = document.querySelectorAll('.list');  // 할일 리스트
+let allBtn = document.querySelector('.btn-all');  // 전체보기
+let doingBtn = document.querySelector('.btn-doing');  // 진행중
+let doneBtn = document.querySelector('.btn-done');  // 완료
+let todoCheck = document.querySelector('.todoCheck');  // 할일 체크(완료)
 
-    // 문서 객체 가져오기
-    const input = document.querySelector('#todo');
-    const todoList = document.querySelector('#todo-list');
-    const addBtn = document.querySelector('#addBtn');
+// 리스트 추가 (enter)
+inputTxt.addEventListener('keyup', function(e) {
 
-    // 변수 선언 (리스트 count)
-    let keyCount = 0;
-
-    // 함수 선언 (할 일 추가)
-    const addTodo = () => {
-
-        // input 칸에 내용이 없으면 경고창
-        if (input.value.trim() === '') {
-            alert('할 일을 입력해주세요');
-            return;
-        }
-
-        // 문서 객체 설정 (할일(item), checkbox, text(할일 리스트), 버튼)
-        const item = document.createElement('div');
-        const checkbox = document.createElement('input');
-        const text = document.createElement('span');
-        const button = document.createElement('button');
-
-        // 문서 객체를 식별할 key 생성
-        const key = keyCount;
-        keyCount += 1;  // 할 일 추가할때마다 증가
-
-        // 할 일 추가시 발생하는 일 
-        // (key, checkbox, 할일, 삭제버튼 추가, 할 일 리스트 생성)
-        item.setAttribute('data-key', key);
-        item.appendChild(checkbox);
-        item.appendChild(text);
-        item.appendChild(button);
-        todoList.appendChild(item);
-
-        // checkbox 객체 (체크하면 취소선 표시)
-        checkbox.type = 'checkbox';
-        checkbox.addEventListener('change', (event) => {
-            item.style.textDecoration = event.target.checked ? 'line-through' : '';
-        })
-
-        // text 객체 (할 일 입력하면 출력)
-        text.textContent = input.value;
-
-        // button 객체 (클릭하면 할 일 삭제)
-        button.textContent = '삭제';
-        button.addEventListener('click', () => {
-            removeTodo(key);
-        })
-
-        // 입력 양식의 내용 비우기 (할일 추가하면 다시 새로고침)
-        input.value = '';
-
+    if (e.key === 'Enter') {
+        addList();
     }
 
-    // 할일 삭제 버튼 클릭시 삭제하기
-    const removeTodo = (key) => {
+    checkList();
+    delList();
+});
 
-        // 식별 키로 문서 객체 제거
-        const item = document.querySelector(`[data-key = "${key}"]`);
-        todoList.removeChild(item);
-
-    }
-
-    // 할 일 추가 (addBtn 누르기, enter키 누르기)
-    addBtn.addEventListener('click', addTodo);
-    input.addEventListener('keyup', (event) => {
-
-        // 입력 양식에서 enter 키 누르면 addTodo 호출
-        const ENTER = 13;
-        if (event.keyCode === ENTER) {
-            addTodo();
-        }
-        
-    })
+// 리스트 추가2
+addBtn.addEventListener('click', function() {
+    
+    addList();
+    checkList();
+    delList();
 })
+
+// inputTxt 입력창 null값 판단
+function addList() {
+
+    if (inputTxt.value !== '') {
+
+        // list를 출력할 <div> 요소 생성
+        let list = document.createElement('div');
+        list.setAttribute('class', 'list');
+        list.innerHTML = `<label class="listLb"><input type="checkbox" class="todoCheck">${inputTxt.value}</label><button class="delBtn">x</button>`;
+
+        input_box.appendChild(list);
+        inputTxt.value = '';
+        inputTxt.getElementsByClassName.borderBottom = '1px solid rgb(163, 155, 155)';
+
+        if (document.querySelector('.notice')) {
+            
+            // 경고메세지 <div>가 생성되는 경우 - 요소 삭제(removeChild)
+            addBox = document.querySelector('.inputSection');
+            let notice = document.querySelector('.notice');
+            addBox.removeChild(notice);
+        }
+        else {
+            // 경고 메시지 <div>가 생성되어있지 않은 경우
+        }
+    }
+    else {
+
+        // 텍스트 미입력시 경고 메세지 출력할 <div> 요소 생성
+        let noticeEl = document.createElement('div');
+        noticeEl.setAttribute('class', 'notice');
+        noticeEl.innerHTML = '<span>내용을 입력해주세요</span>';
+
+        inputTxt.style.borderBottom = '2px solid rgb(201, 65, 65)';
+
+        if (document.querySelector('.notice')) {
+            // 경고 메시지 <div>가 생성되어 있는 경우
+        }
+        else {
+            // 경고 메세지 <div>가 생성되어있지 않은 경우 - 요소추가 (appendChild)
+            addbox = document.querySelector('.inputSection');
+            addbox.appendChild(noticeEl);
+        }
+    }
+}
+
+// 리스트 체크
+function checkList() {
+
+    todoCheck = document.querySelectorAll('.todoCheck');
+    let listLb = document.querySelectorAll('.listLb');
+
+    todoCheck.forEach((listEl, index) => listEl.addEventListener('click', function() {
+
+        if (listEl.checked === true) {
+            listLb[index].style.textDecoration = 'line-through';
+        }
+        else {
+            listLb[index].style.textDecoration = 'none';
+        }
+
+        // listCount();
+    }))
+}
+
+// 리스트 삭제;
+function delList() {
+
+    delTodo = document.querySelectorAll('.delBtn');
+    todoList = document.querySelectorAll('.list');
+
+    delTodo.forEach((delEl, index) => delEl.addEventListener('click', function() {
+
+        todoList[index].remove();
+    }))
+
+    // listCount();
+}
+
+// 전체조회
+addBtn.addEventListener('click', function() {
+
+    todoList = document.querySelector('.list');
+
+    todoList.forEach(listEl => {
+        listEl.style.display = '';
+    });
+});
+
+// 진행중
+doingBtn.addEventListener('click', function() {
+
+    todoList = document.querySelector('.list');
+
+    for (let i = 0; i < todoCheck.length; i++) {
+
+        if (todoCheck[i].checked === true) {
+            todoList[i].style.display = 'none';
+        }
+        else {
+            todoList[i].style.display = '';
+        }
+    }
+});
+
+// 완료
+doneBtn.addEventListener('click', function() {
+
+    todoList = document.querySelectorAll('.list');
+
+    for (let i = 0; i < todoCheck.length; i++) {
+
+        if (todoCheck[i].checked === true) {
+            todoList[i].style.display = '';
+        }
+        else {
+            todoList[i].style.display = 'none';
+        }
+    }
+});
